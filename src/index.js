9368d6bd91821bb11095d2780596f924f894d2d3
Emilly Pinheiro <esilvapinheiro2006@gmail.com>
2025-09-20 16:50:27 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import { adicionarRotas } from './rotas.js';
+
+import express from 'express'
+const api = express();
+api.use(express.json());
+
+adicionarRotas(api);
+
+api.listen(6110, () => console.log('..: API subiu com sucesso'))
+
